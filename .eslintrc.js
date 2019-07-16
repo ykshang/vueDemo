5440596acb3a768c3a71bc4e281b48148ed9e112
shangyukang <shangyukang@inspur.com>
2019-07-16 16:25:16 +0800

    提交eslint验证规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,43 @@
 // https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
-    parser: 'babel-eslint'
+    sourceType: 'module'
   },
-  env: {
-    browser: true,
-  },
-  extends: [
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
-  ],
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
   // required to lint *.vue files
   plugins: [
-    'vue'
+    'html'
   ],
   // add your custom rules here
-  rules: {
+  'rules': {
+    "indent": ['warn', 2],  //warn error
+    "no-unused-vars": [0, { "vars": "all", "args": "none" }],
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    'no-undef': 0,
+    'spaced-comment': [0, "never"],
     // allow async-await
-    'generator-star-spacing': 'off',
+    'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'avoidEscape':true,
-    //不检查句尾分号
-    'semi':[1,"always"],
-    //总是检查句尾分号，并给出错误提示。
-    //'semi': ["error", "always"]
+    //'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-debugger': 2,
+    'semi': [1, "always"],
+    'func-call-spacing': [0, "never"],
+    "space-before-function-paren": [0, "always"],
+    "key-spacing": ["warn", { "afterColon": true }],
+    "quotes": [0, "single"],
+    "eqeqeq": ["warn", "always"],
+    "space-in-parens": [0, "never"],
+    "space-infix-ops": [0, {"int32Hint": false}],
+    "yoda": [0, "never", { "exceptRange": true }],
+    "handle-callback-err": [0, "error"],
+    "no-extra-bind": [0, "error"],
+    "no-eval": 0,
+    "no-useless-escape": 0,
+    // allow async-await
+    'generator-star-spacing': 0,
   }
 }
