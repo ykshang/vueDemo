a41e1aa2b541b81871381520b0e83e3497eda1ab
shangyukang <shangyukang@inspur.com>
2020-03-01 22:47:59 +0800

    增加模块：增加右上角头像

--- src/views/headers/Header.vue
+++ src/views/headers/Header.vue
@@ -4,6 +4,9 @@
     <el-image class="logoStyle" :src="logoSrc" :fit="'contain'">
     </el-image>
   </div>
+  <div class="avatar">
+    <el-avatar :src="avatarSrc"></el-avatar>
+  </div>
 </div>
 </template>
 
@@ -13,7 +16,8 @@ export default {
   name: 'Header',
   data () {
     return {
-      logoSrc: logo
+      logoSrc: logo,
+      avatarSrc: "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
     };
   },
   mounted () {
@@ -25,7 +29,12 @@ export default {
 <style scoped>
 .logo {
   margin: 15px 0px;
+  float: left;
   height: 30px;
   width: 168px;
+}
+.avatar {
+  float: right;
+  margin: 8px 20px;
 }
 </style>
