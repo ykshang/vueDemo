3a86cfe31ce0bde59311de895736f57a612bcce3
kangkang shang <535634238@qq.com>
2025-08-08 00:12:54 +0800

    props的ts支持

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,16 +1,32 @@
 <template>
-  <person ref="ren"></person>
-  <button @click="viewRen">查看 ren</button>
+  <div class="container">
+    <person ref="ren" :aaa="personList" bbb="111"></person>
+    <!-- <button @click="viewRen">查看 ren</button> -->
+  </div>
 </template>
 <script lang="ts" setup>
 // import { RouterLink, RouterView } from 'vue-router'
-import { defineOptions, ref } from 'vue'
+import { defineOptions, ref, reactive } from 'vue'
 import Person from './components/Person.vue'
+import { type PersonList } from '@/types/index'
+
 defineOptions({ name: 'App' })
 const ren = ref()
-function viewRen() {
-  console.log(ren.value)
-}
+
+const personList = reactive<PersonList>([
+  { id: '1', age: 21, name: 'zhangsan' },
+  { id: '2', age: 22, name: 'lisi' },
+  { id: '3', age: 24, name: 'wangwu' },
+])
+// function viewRen() {
+//   console.log(ren.value)
+// }
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- app/src/components/Person.vue
+++ app/src/components/Person.vue
@@ -1,35 +1,40 @@
 <template>
   <div class="person">
-    <h1 ref="title1">尚硅谷</h1>
-    <h2 ref="title2">前端</h2>
-    <h3 ref="title3">Vue</h3>
-    <button @click="viewRef">查看 ref</button>
+    <div>
+      <div v-for="item in aaa" :key="item.id">
+        <span> name：{{ item.name }}, age: {{ item.age }} </span>
+      </div>
+    </div>
+    <div>{{ bbb }}</div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, defineOptions } from 'vue'
-import { type PersonInterface, type PersonList } from '@/types/index'
+import { reactive, withDefaults } from 'vue'
+import { type PersonList } from '@/types/index'
 defineOptions({ name: 'person-1' })
-
-const person: PersonInterface = {
-  id: '1',
-  age: 1,
-  name: 'kangkang',
+// 接收参数
+// defineProps(['aaa', 'bbb'])
+interface Props {
+  aaa: PersonList
+  bbb?: string
 }
-console.clear()
-console.log(person)
-const personList: PersonList = []
+// const props = defineProps<Props>()
+const props = withDefaults(defineProps<Props>(), {
+  aaa: () => [{ id: '1', age: 21, name: 'zhangsan' }],
+})
+console.log(props)
+const personList = reactive<PersonList>([
+  { id: '1', age: 21, name: 'zhangsan' },
+  { id: '2', age: 22, name: 'lisi' },
+  { id: '3', age: 24, name: 'wangwu' },
+])
 // const personList: PersonInterface[] = []
-personList.push(person)
 console.log(personList)
-const title1 = ref()
-const title2 = ref()
-const title3 = ref()
-// 通过ref获取元素
-function viewRef() {
-  console.log(title1.value)
-  console.log(title2.value)
-  console.log(title3.value)
-}
 </script>
+<style scoped>
+.person {
+  display: flex;
+  padding: 30px;
+}
+</style>
