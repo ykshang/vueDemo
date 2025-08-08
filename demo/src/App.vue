37236ef788ae4a11e562bee66258faccd05cc1ee
kangkang shang <535634238@qq.com>
2025-08-09 01:38:14 +0800

    基本路由都封装

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,85 +1,54 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="common-layout">
+    <el-container>
+      <el-header class="">
+        <el-button
+          type="primary"
+          v-for="button in buttonList"
+          :key="button.id"
+          @click="toView(button.path)"
+          >{{ button.name }}</el-button
+        >
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script setup lang="ts">
+import { reactive } from 'vue'
+import { RouterView, useRouter } from 'vue-router'
+const buttonList = reactive([
+  { id: 1, name: 'home', path: '/' },
+  { id: 1, name: 'about', path: '/about' },
+  { id: 2, name: 'news', path: '/news' },
+])
+const router = useRouter();
+function toView(path: string) {
+  router.push(path);
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+<style lang="scss" scoped>
+.common-layout {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
+.common-layout .el-header,
+.common-layout .el-footer {
+  background-color: var(--el-color-primary-light-7);
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.common-layout .el-main {
+  background-color: var(--el-color-primary-light-9);
+  color: var(--el-text-color-primary);
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  flex: 1;
 }
 </style>
