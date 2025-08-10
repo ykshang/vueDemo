0536fea885234f79087c3bbf17346ba55176cec1
kangkang shang <535634238@qq.com>
2025-08-10 21:24:14 +0800

    保存

--- demo/src/stores/counter.ts
+++ demo/src/stores/counter.ts
@@ -10,3 +10,5 @@ export const useCounterStore = defineStore('counter', () => {
 
   return { count, doubleCount, increment }
 })
+
+
--- demo/src/views/HomeView.vue
+++ demo/src/views/HomeView.vue
@@ -2,28 +2,29 @@
   <div>
     <el-button type="primary" @click="jump">跳转到 news</el-button>
     <!-- <el-button type="primary" @click="counterStore.increment()"> +1 </el-button> -->
-    <!-- <div>{{ count }}</div> -->
-    <!-- <div>{{ doubleCount }}</div> -->
-    <el-button type="primary" @click="countStore.incrementCount"> count +1 </el-button>
-    <div>{{ countStore.count }}</div>
+    <div>{{ count }}</div>
+    <div>{{ doubleCount }}</div>
+    <!-- <el-button type="primary" @click="countStore.incrementCount"> count +1 </el-button> -->
+    <!-- <div>{{ countStore.count }}</div> -->
     <div>{{ userInfoStore.name }}</div>
     <div>{{ userInfoStore.age }}</div>
   </div>
 </template>
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
-// import { useCounterStore } from '@/stores/counter'
-import { useCountStore } from '@/stores/count'
+import { useCounterStore } from '@/stores/counter'
+// import { useCountStore } from '@/stores/count'
 import { useUserInfoStore } from '@/stores/user'
-// import { storeToRefs } from 'pinia'
-// import { toRefs } from 'vue'
+import { storeToRefs } from 'pinia'
+import { toRefs } from 'vue'
 
-// const counterStore = useCounterStore()
-const countStore = useCountStore()
+const counterStore = useCounterStore()
+// const countStore = useCountStore()
 const userInfoStore = useUserInfoStore()
-// const { count, doubleCount } = storeToRefs(counterStore)
-// console.log('counterStore', toRefs(counterStore))
-console.log('countStore', countStore)
+const { count, doubleCount } = storeToRefs(counterStore)
+console.log('toRefs', toRefs(counterStore))
+console.log('storeToRefs', storeToRefs(counterStore))
+// console.log('countStore', countStore)
 
 const router = useRouter()
 function jump() {
