88ccd83c6b77a1a18a2d0066f5cd59843cc921bf
kangkang shang <535634238@qq.com>
2025-08-14 21:17:56 +0800

    chore: 更新commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,7 @@
 export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore'],
-      '提交类型不能为空：新功能 feat、fix 修复、docs 文档、style 格式、refactor 重构、test 测试、chore 构建/杂项',
-    ],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
     'subject-empty': [2, 'never', '提交主题不能为空'],
     'type-empty': [2, 'never', '提交类型不能为空'],
   },
