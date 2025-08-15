be3dcd2bf997c199b8a5eb242eb3b36c520b207e
kangkang shang <535634238@qq.com>
2025-08-16 00:24:03 +0800

    feat: 新图标组件

---
+++ demo/src/components/icons/index.ts
@@ -0,0 +1,17 @@
+import IconCommunity from './IconCommunity.vue'
+import IconDark from './IconDark.vue'
+import IconDocumentation from './IconDocumentation.vue'
+import IconEcosystem from './IconEcosystem.vue'
+import IconLight from './IconLight.vue'
+import IconSupport from './IconSupport.vue'
+import IconTooling from './IconTooling.vue'
+
+export {
+  IconDark,
+  IconLight,
+  IconDocumentation,
+  IconCommunity,
+  IconEcosystem,
+  IconSupport,
+  IconTooling,
+}
