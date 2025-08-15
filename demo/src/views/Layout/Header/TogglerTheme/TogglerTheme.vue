c8cb7a05e6d2f9f2782ec909fdf5c1a4edde2a93
kangkang shang <535634238@qq.com>
2025-08-15 19:54:11 +0800

    feat: 完成主题切换动画

--- demo/src/views/Layout/Header/TogglerTheme/TogglerTheme.vue
+++ demo/src/views/Layout/Header/TogglerTheme/TogglerTheme.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-switch v-model="darkMode">
+  <el-switch ref="switchRef" v-model="darkMode" :before-change="beforeChange">
     <template #active-action>
       <DarkIcon />
     </template>
@@ -10,12 +10,14 @@
 </template>
 <script lang="ts" setup>
 import { isDark, toggleDark } from '@/composables/dark'
+import type { SwitchInstance } from 'element-plus'
 import DarkIcon from './Dark.vue'
 import LightIcon from './Light.vue'
-import { ref, watch } from 'vue'
+import { ref, watch, nextTick } from 'vue'
 defineOptions({
   name: 'TogglerTheme',
 })
+const switchRef = ref<SwitchInstance>()
 
 const darkMode = ref(isDark.value)
 watch(
@@ -33,6 +35,56 @@ watch(
     }
   }
 )
+const beforeChange = () => {
+  return new Promise<boolean>((resolve) => {
+    const isAppearanceTransition =
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      document.startViewTransition &&
+      !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+    if (!isAppearanceTransition) {
+      resolve(true)
+      return
+    }
+
+    const switchElement = switchRef.value?.$el
+    const rect = switchElement.getBoundingClientRect()
+    const x = rect.left + rect.width / 2
+    const y = rect.top + rect.height / 2
+
+    const endRadius = Math.hypot(
+      Math.max(x, innerWidth - x),
+      Math.max(y, innerHeight - y)
+    )
+
+    const ratioX = (100 * x) / innerWidth
+    const ratioY = (100 * y) / innerHeight
+    const referR = Math.hypot(innerWidth, innerHeight) / Math.SQRT2
+    const ratioR = (100 * endRadius) / referR
+    const transition = document.startViewTransition(async () => {
+      resolve(true)
+      await nextTick()
+    })
+    transition.ready.then(() => {
+      const clipPath = [
+        `circle(0% at ${ratioX}% ${ratioY}%)`,
+        `circle(${ratioR}% at ${ratioX}% ${ratioY}%)`,
+      ]
+      document.documentElement.animate(
+        {
+          clipPath: isDark.value ? [...clipPath].reverse() : clipPath,
+        },
+        {
+          duration: 400,
+          easing: 'ease-in',
+          pseudoElement: isDark.value
+            ? '::view-transition-old(root)'
+            : '::view-transition-new(root)',
+        }
+      )
+    })
+  })
+}
 </script>
 <style lang="scss" scoped>
 :deep(.el-switch__core) {
@@ -61,4 +113,26 @@ watch(
 :deep(.light-icon) {
   color: #606266;
 }
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+
+::view-transition-old(root) {
+  z-index: 1;
+}
+
+::view-transition-new(root) {
+  z-index: 2147483646;
+}
+
+.dark::view-transition-old(root) {
+  z-index: 2147483646;
+}
+
+.dark::view-transition-new(root) {
+  z-index: 1;
+}
+
 </style>
