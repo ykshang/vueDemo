52d8137dc21254e9391d1795ba9bf9d9f0161e67
kangkang shang <535634238@qq.com>
2025-08-22 22:03:17 +0800

    feat: header 代码拆分，样式调整

--- app/vue-admin/src/components/layouts/Header/BaseHeader.vue
+++ app/vue-admin/src/components/layouts/Header/BaseHeader.vue
@@ -1,78 +1,12 @@
 <script lang="ts" setup>
-import { ArrowDown } from '@element-plus/icons-vue'
-import { storeToRefs } from 'pinia'
-import { useRouter } from 'vue-router'
-import { toggleDark } from '~/composables/util'
-import { useUserInfoStore } from '~/stores/userIfnoStroe'
-
-const router = useRouter()
-
-function loginout() {
-  router.push('/Login')
-}
-/**
- * 获取当前已登录的用户信息
- */
-const userInfoStore = useUserInfoStore()
-const { currentUserInfo } = storeToRefs(userInfoStore)
 </script>
 
 <template>
-  <div class="header h-66px bg-[--ep-bg-color]">
+  <div h-66px flex items-center b-b-1px b-b-solid class="b-b-color-[--ep-menu-border-color] bg-[--ep-bg-color]">
     <MenuCollapseButton />
     <div flex-1>
       {{ '' }}
     </div>
-    <div class="mr-15px flex items-center">
-      <!-- <IconAvatar mr-10px h-30px w-30px /> -->
-      <el-avatar shape="circle" :src="currentUserInfo.avatarUrl" mr-10px cursor-pointer />
-      <el-dropdown>
-        <span class="el-dropdown-link">
-          <span class="username">
-            {{ currentUserInfo.userName }}
-            <el-icon ml-5px>
-              <ArrowDown />
-            </el-icon>
-          </span>
-        </span>
-        <template #dropdown>
-          <el-dropdown-menu>
-            <el-dropdown-item>
-              <span w-80px text-center>个人中心</span>
-            </el-dropdown-item>
-            <el-dropdown-item>
-              <span w-80px text-center @click="toggleDark()">切换主题</span>
-            </el-dropdown-item>
-            <el-dropdown-item>
-              <span w-80px text-center @click="loginout">退出登录</span>
-            </el-dropdown-item>
-          </el-dropdown-menu>
-        </template>
-      </el-dropdown>
-    </div>
+    <UserOperationBar />
   </div>
 </template>
-
-<style lang="scss">
-.header {
-  display: flex;
-  border-bottom: solid 1px var(--ep-menu-border-color);
-  align-items: center;
-
-  .el-dropdown-link {
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    margin-right: 10px;
-
-    // color: #fff;
-    .username {
-      font-size: 14px;
-
-      &:hover {
-        color: var(--el-color-primary);
-      }
-    }
-  }
-}
-</style>
---
+++ app/vue-admin/src/components/layouts/Header/UserOperationBar.vue
@@ -0,0 +1,57 @@
+<script lang="ts" setup>
+import { storeToRefs } from 'pinia'
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { toggleDark } from '~/composables/util'
+import { useUserInfoStore } from '~/stores/userIfnoStroe'
+
+const router = useRouter()
+
+function loginout() {
+  router.push('/Login')
+}
+
+/**
+ * 获取当前已登录的用户信息
+ */
+const userInfoStore = useUserInfoStore()
+const { currentUserInfo } = storeToRefs(userInfoStore)
+
+/**
+ * 下拉菜单显示状态改变时触发
+ */
+const isVisible = ref(false)
+function handleVisibleChange(visible: boolean) {
+  isVisible.value = visible
+}
+</script>
+
+<template>
+  <div class="mr-15px flex items-center">
+    <el-avatar shape="circle" :src="currentUserInfo.avatarUrl" class="mr-8px cursor-pointer" />
+    <el-dropdown @visible-change="handleVisibleChange">
+      <span
+        class="mr-10px max-w-150px min-w-50px flex cursor-pointer items-center font-size-14px hover:color-[--ep-color-primary]"
+      >
+        <span class="overflow-hidden text-ellipsis whitespace-nowrap">{{ currentUserInfo.userName }}</span>
+        <div class="ml-5px inline-block">
+          <div v-if="!isVisible" class="i-ep:arrow-down" />
+          <div v-else class="i-ep:arrow-up" />
+        </div>
+      </span>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>
+            <span class="w-80px text-center">个人中心</span>
+          </el-dropdown-item>
+          <el-dropdown-item>
+            <span class="w-80px text-center" @click="toggleDark()">切换主题</span>
+          </el-dropdown-item>
+          <el-dropdown-item>
+            <span w-80px text-center @click="loginout">退出登录</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+  </div>
+</template>
