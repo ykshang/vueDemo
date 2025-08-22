5bc04520d8d0efed78a80c7776443f94da18bc24
kangkang shang <535634238@qq.com>
2025-08-22 15:44:43 +0800

    chore: 修改eslint规则，空元素结尾必须斜线

--- app/vue-admin/eslint.config.js
+++ app/vue-admin/eslint.config.js
@@ -6,5 +6,17 @@ export default antfu({
   vue: true,
   rules: {
     'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
   },
 })
