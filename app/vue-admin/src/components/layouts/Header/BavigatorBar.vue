cd96b8bb5cbdfecd53e96056b86f9718c7620ae5
kangkang shang <535634238@qq.com>
2025-08-24 22:51:33 +0800

    feat: 在路由中获取参数，动态刷新导航栏

---
+++ app/vue-admin/src/components/layouts/Header/BavigatorBar.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import menuData from '../Aside/menu-data'
+
+const route = useRoute()
+
+const navitemList = computed(() => {
+  const routeList = route.matched.slice(1) || []
+  const result: any[] = []
+  let menuList = menuData
+  while (routeList.length > 0) {
+    const route = routeList.shift()
+    const menu = menuList.find((menu) => {
+      return menu.index === route?.path
+    })
+    result.push(menu)
+    menuList = menu?.subMenu as any
+  }
+  return result
+})
+</script>
+
+<template>
+  <el-breadcrumb class="ml-20px" separator=">">
+    <el-breadcrumb-item v-for="nav in navitemList" :key="nav?.path">
+      <div class="flex cursor-pointer items-center justify-center">
+        <component :is="nav.icon" class="h-16px w-16px" />
+        <div class="ml-3px font-size-14px">
+          {{ nav?.title }}
+        </div>
+      </div>
+    </el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<style scoped lang="scss"></style>
