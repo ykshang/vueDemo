3c0dbc5842cb428a3cb3b6c8e3fe2849fa37aad8
kangkang shang <535634238@qq.com>
2025-08-26 00:47:12 +0800

    build: 调整文件名

--- app/vue-admin/src/pages/index.vue
+++ app/vue-admin/src/pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <el-config-provider namespace="ep">
-    <LayoutSide />
+    <layout-side />
     <div class="main-container flex flex-1 flex-col overflow-hidden bg-[--ep-bg-color-page]">
-      <LayoutHeader />
+      <layout-header />
       <RouterView />
     </div>
   </el-config-provider>
