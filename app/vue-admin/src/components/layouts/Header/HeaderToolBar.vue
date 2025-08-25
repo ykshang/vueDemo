a886a262b43d337aeda8cb50f85a6ffb8b863cd5
kangkang shang <535634238@qq.com>
2025-08-25 22:07:16 +0800

    feat: 添加全屏切换功能

--- app/vue-admin/src/components/layouts/Header/HeaderToolBar.vue
+++ app/vue-admin/src/components/layouts/Header/HeaderToolBar.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+import { useFullscreen } from '@vueuse/core'
+
+// 切换全屏
+const target = document.getElementById('app') as HTMLElement
+const { isFullscreen, toggle } = useFullscreen(target)
+</script>
+
 <template>
   <div mr-16px flex items-center justify-center>
     <div class="icons">
@@ -7,6 +15,10 @@
       <el-badge :value="3" class="item">
         <div i-ep-bell class="icon" />
       </el-badge>
+    </div>
+    <div class="icons">
+      <div v-if="!isFullscreen" class="i-ri:fullscreen-line icon" @click="toggle" />
+      <div v-else class="i-ri:fullscreen-exit-fill icon" @click="toggle" />
     </div>
     <div class="icons">
       <div i-ri-brush-3-line class="icon" />
