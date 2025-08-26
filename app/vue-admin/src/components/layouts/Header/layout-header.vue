8e6a2657e07b36fea80f493f72072e87ad09c513
kangkang shang <535634238@qq.com>
2025-08-26 16:23:48 +0800

    style: 调整header的样式

--- app/vue-admin/src/components/layouts/Header/layout-header.vue
+++ app/vue-admin/src/components/layouts/Header/layout-header.vue
@@ -2,14 +2,26 @@
 </script>
 
 <template>
-  <div class="h-60px flex items-center b-b-1px b-b-color-[--ep-menu-border-color] b-b-solid bg-[--ep-bg-color]">
+  <div class="border-bottom h-60px min-h-60px flex items-center">
+    <!-- 控制左侧菜单的展开和收起 -->
     <header-menu-collapse-btn />
+    <!-- 导航栏 -->
     <header-navigator-bar />
     <div flex-1 />
+    <!-- 头像左侧的工具条 -->
     <header-tool-bar />
+    <!-- 头像和用户信息 -->
     <header-user-operation-bar />
   </div>
-  <div class="h-50px flex items-center b-b-1px b-b-color-[--ep-menu-border-color] b-b-solid bg-[--ep-bg-color]">
+  <div class="border-bottom pos-relative h-50px min-h-50px px-10px">
+    <!-- 标签页 -->
     <header-tabs />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.border-bottom {
+  border-bottom: 1px solid var(--ep-menu-border-color);
+  background-color: var(--ep-bg-color);
+}
+</style>
