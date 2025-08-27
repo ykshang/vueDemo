12cd1c8d3c5de30f41c196609b11ba52aa0b9e00
kangkang shang <535634238@qq.com>
2025-08-27 20:46:14 +0800

    feat(Home/Demo): 添加列表操作功能
    在 Demo.vue 中添加响应式列表及其操作按钮，包括添加元素和过滤元素功能

--- app/vue-admin/src/pages/index/Home/Demo.vue
+++ app/vue-admin/src/pages/index/Home/Demo.vue
@@ -1,5 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const list = ref([1, 2, 3])
+
+function handlePush() {
+  list.value.push(4) // 响应式更新
+}
+
+function handleFilter() {
+  list.value = list.value.filter(v => v > 2) // 需重新赋值
+}
+</script>
+
 <template>
-  <div>
-    Demo
-  </div>
+  <button @click="handlePush">
+    添加元素
+  </button>
+  <button @click="handleFilter">
+    过滤元素
+  </button>
+  <ul>
+    <li v-for="item in list" :key="item">
+      {{ item }}
+    </li>
+  </ul>
 </template>
