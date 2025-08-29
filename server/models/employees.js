370d7e708ac760222a7aecf0c045f015564c469c
kangkang shang <535634238@qq.com>
2025-08-29 12:49:00 +0800

    feat(员工模型): 添加员工数据模型
    添加员工数据模型，包含姓名、邮箱、部门、岗位、手机号和密码等字段，用于员工信息管理

---
+++ server/models/employees.js
@@ -0,0 +1,16 @@
+import mongoose from '../db.js' // 复用连接
+
+// 定义模型
+const dataSchema = new mongoose.Schema({
+  firstName: String, // 姓
+  lastName: String, // 名
+  fullName:String, // 全名
+  email: String, // 邮箱
+  dept: String, // 部门
+  jobPostion: String, // 岗位
+  phone: String, // 手机号
+  password: String, // 密码
+})
+
+// 导出模型
+export default mongoose.model('employees', dataSchema)
