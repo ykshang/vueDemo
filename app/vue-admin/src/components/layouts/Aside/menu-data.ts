2323263f1e912e3ae52c17471c9e388396c6ac32
kangkang shang <535634238@qq.com>
2025-08-29 15:58:25 +0800

    perf(菜单): 使用异步组件优化菜单图标加载性能
    将菜单图标从静态导入改为异步加载，使用defineAsyncComponent和markRaw优化性能
    添加字典管理菜单项

--- app/vue-admin/src/components/layouts/Aside/menu-data.ts
+++ app/vue-admin/src/components/layouts/Aside/menu-data.ts
@@ -1,82 +1,100 @@
-import { ref } from 'vue'
-
-import RiAlarmWarningLine from '~icons/ri/alarm-warning-line'
-import RiAppsLine from '~icons/ri/apps-line'
-import RiBarChartBoxAiLine from '~icons/ri/bar-chart-box-ai-line'
-import RiBox3Line from '~icons/ri/box-3-line'
-import RiBuildingLine from '~icons/ri/building-line'
-import RiCalendarTodoLine from '~icons/ri/calendar-todo-line'
-import RiGroupLine from '~icons/ri/group-line'
-import RiHome2Line from '~icons/ri/home-2-line'
-import RiHome3Line from '~icons/ri/home-3-line'
-import RiRemixiconLine from '~icons/ri/remixicon-line'
-import RiUser3Line from '~icons/ri/user-3-line'
-import RiUserSettingsLine from '~icons/ri/user-settings-line'
+import { defineAsyncComponent, markRaw } from 'vue'
 
 const menuDataTree = [
   {
     path: '/Home',
     title: '首页',
-    icon: RiHome3Line,
+    icon: markRaw(defineAsyncComponent(() =>
+      import('~icons/ri/home-3-line'),
+    )),
     subMenu: [
       {
         path: '/Home/HomePage',
         title: '欢迎页',
-        icon: RiHome2Line,
+        icon: markRaw(defineAsyncComponent(() =>
+          import('~icons/ri/home-2-line'),
+        )),
       },
       {
         path: '/Home/DashBoard',
         title: '数据看板',
-        icon: RiBarChartBoxAiLine,
+        icon: markRaw(defineAsyncComponent(() =>
+          import('~icons/ri/bar-chart-box-ai-line'),
+        )),
       },
       {
         path: '/Home/Demo',
         title: '测试 Demo',
-        icon: RiAlarmWarningLine,
+        icon: markRaw(defineAsyncComponent(() =>
+          import('~icons/ri/alarm-warning-line'),
+        )),
       },
     ],
   },
   {
     path: '/Setting',
     title: '配置中心',
-    icon: RiUserSettingsLine,
+    icon: markRaw(defineAsyncComponent(() =>
+      import('~icons/ri/user-settings-line'),
+    )),
     subMenu: [
       {
         path: '/Setting/Profile',
         title: '个人信息',
-        icon: RiUser3Line,
+        icon: markRaw(defineAsyncComponent(() =>
+          import('~icons/ri/user-3-line'),
+        )),
       },
       {
         path: '/Setting/EmployeeManagent',
         title: '员工管理',
-        icon: RiGroupLine,
+        icon: markRaw(defineAsyncComponent(() =>
+          import('~icons/ri/group-line'),
+        )),
       },
       {
         path: '/Setting/DepartmentMangement',
         title: '部门管理',
-        icon: RiBuildingLine,
+        icon: markRaw(defineAsyncComponent(() =>
+          import('~icons/ri/building-line'),
+        )),
       },
       {
         path: '/Setting/MenuMangement',
         title: '菜单管理',
-        icon: RiAppsLine,
+        icon: markRaw(defineAsyncComponent(() =>
+          import('~icons/ri/apps-line'),
+        )),
       },
       {
         path: '/Setting/TaskMangement',
         title: '任务管理',
-        icon: RiCalendarTodoLine,
+        icon: markRaw(defineAsyncComponent(() =>
+          import('~icons/ri/calendar-todo-line'),
+        )),
+      },
+      {
+        path: '/Setting/DictionaryManagent',
+        title: '字典管理',
+        icon: markRaw(defineAsyncComponent(() =>
+          import('~icons/ri/book-2-line'),
+        )),
       },
     ],
   },
   {
     path: '/Components',
     title: '内置组件',
-    icon: RiBox3Line,
+    icon: markRaw(defineAsyncComponent(() =>
+      import('~icons/ri/box-3-line'),
+    )),
     subMenu: [
       {
         path: '/Components/IconList',
         title: '图标组件',
-        icon: RiRemixiconLine,
+        icon: markRaw(defineAsyncComponent(() =>
+          import('~icons/ri/remixicon-line'),
+        )),
       },
     ],
   },
@@ -84,7 +102,9 @@ const menuDataTree = [
 const defaultHomePage = {
   path: '/Home/HomePage',
   title: '欢迎页',
-  icon: RiHome2Line,
+  icon: markRaw(defineAsyncComponent(() =>
+    import('~icons/ri/home-2-line'),
+  )),
   readonly: true,
 }
 const menuDataList = menuDataTree.flatMap(item => item.subMenu)
