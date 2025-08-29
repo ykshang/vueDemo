d2bd70ae7f8d7d7d5d459d3b9c4d227b92e447ec
kangkang shang <535634238@qq.com>
2025-08-29 21:16:56 +0800

    feat(字典管理): 实现字典列表展示功能并优化页面布局
    - 添加表格数据绑定及初始化加载逻辑
    - 移除测试代码和冗余表单字段
    - 新增"新增"按钮并调整页面布局
    - 更新表格列配置以匹配字典数据结构

--- app/vue-admin/src/pages/index/Setting/DictionaryManagent.vue
+++ app/vue-admin/src/pages/index/Setting/DictionaryManagent.vue
@@ -1,25 +1,27 @@
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
 import dictionaryService from '~/composables/services/dictionaryService'
 
-const request: Dictionary = {
-  dictionaryKey: 'test',
-  name: '测试字典',
-  desc: '测试字典',
+// dictionaryService.createDictionary(request).then((res) => {
+
+//   // console.log('字典列表', res)
+//   // if (res.status === 'success') {
+//   // console.log('字典列表', res.data)
+//   // } else {
+//   // console.log('获取字典列表失败', res.msg)
+//   // }
+// })
+const tableData = ref<Dictionary[]>([])
+// 初始化加载表格数据
+initTableData()
+function initTableData() {
+  dictionaryService.getDictionaryList().then((res: any) => {
+    // console.log('字典列表', res)
+    if (res.status === 'success') {
+      tableData.value = res.data
+    }
+  })
 }
-dictionaryService.createDictionary(request).then((res) => {
-
-  // console.log('字典列表', res)
-  // if (res.status === 'success') {
-  // console.log('字典列表', res.data)
-  // } else {
-  // console.log('获取字典列表失败', res.msg)
-  // }
-})
-dictionaryService.getDictionaryList().then((res) => {
-
-  // console.log('字典列表', res)
-})
 const formInline = reactive({
   key: '',
   name: '',
@@ -32,12 +34,14 @@ function onSubmit() {
 <template>
   <div class="page-container">
     <div class="page-container-header">
+      <div flex flex-1>
+        <el-button type="primary">
+          新增
+        </el-button>
+      </div>
       <el-form :inline="true" :model="formInline" class="demo-form-inline">
         <el-form-item label="字典 Key">
           <el-input v-model="formInline.key" placeholder="请输入字典关键词" clearable />
-        </el-form-item>
-        <el-form-item label="字典名称">
-          <el-input v-model="formInline.name" placeholder="请输入字典名称" clearable />
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="onSubmit">
@@ -47,10 +51,11 @@ function onSubmit() {
       </el-form>
     </div>
     <div class="page-container-content">
-      <el-table :data="[]" style="width: 100%">
-        <el-table-column prop="date" label="日期" width="180" />
-        <el-table-column prop="name" label="姓名" width="180" />
-        <el-table-column prop="address" label="地址" />
+      <el-table :data="tableData" style="width: 100%">
+        <el-table-column type="index" width="50" />
+        <el-table-column prop="dictionaryKey" label="关键字" width="180" />
+        <el-table-column prop="name" label="字典名称" width="180" />
+        <el-table-column prop="desc" label="描述" />
       </el-table>
     </div>
   </div>
