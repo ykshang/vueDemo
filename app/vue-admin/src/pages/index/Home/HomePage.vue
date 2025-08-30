c67099f8742749f6b8d57ec66d37f7d0764cbb04
kangkang shang <535634238@qq.com>
2025-08-30 16:43:59 +0800

    refactor(HomePage): 添加外层容器并调整布局结构
    将页面内容包裹在 flex-1 容器中，保持原有功能不变的同时改善布局结构

--- app/vue-admin/src/pages/index/Home/HomePage.vue
+++ app/vue-admin/src/pages/index/Home/HomePage.vue
@@ -10,55 +10,57 @@ const value1 = ref(true)
 </script>
 
 <template>
-  <el-row :gutter="20">
-    <el-col :span="24">
-      <Welcome />
-    </el-col>
-  </el-row>
-  <div class="my-2 flex flex-wrap items-center justify-center text-center">
-    <el-button @click="count++">
-      count is: {{ count }}
-    </el-button>
-    <el-button type="primary" @click="count++">
-      count is: {{ count }}
-    </el-button>
-    <el-button type="success" @click="count++">
-      count is: {{ count }}
-    </el-button>
-    <el-button type="warning" @click="count++">
-      count is: {{ count }}
-    </el-button>
-    <el-button type="danger" @click="count++">
-      count is: {{ count }}
-    </el-button>
-    <el-button type="info" @click="count++">
-      count is: {{ count }}
-    </el-button>
-  </div>
+  <div flex-1>
+    <el-row :gutter="20">
+      <el-col :span="24">
+        <Welcome />
+      </el-col>
+    </el-row>
+    <div class="my-2 flex flex-wrap items-center justify-center text-center">
+      <el-button @click="count++">
+        count is: {{ count }}
+      </el-button>
+      <el-button type="primary" @click="count++">
+        count is: {{ count }}
+      </el-button>
+      <el-button type="success" @click="count++">
+        count is: {{ count }}
+      </el-button>
+      <el-button type="warning" @click="count++">
+        count is: {{ count }}
+      </el-button>
+      <el-button type="danger" @click="count++">
+        count is: {{ count }}
+      </el-button>
+      <el-button type="info" @click="count++">
+        count is: {{ count }}
+      </el-button>
+    </div>
 
-  <div>
-    <el-tag type="success" class="m-1">
-      Tag 1
-    </el-tag>
-    <el-tag type="warning" class="m-1">
-      Tag 1
-    </el-tag>
-    <el-tag type="danger" class="m-1">
-      Tag 1
-    </el-tag>
-    <el-tag type="info" class="m-1">
-      Tag 1
-    </el-tag>
-  </div>
+    <div>
+      <el-tag type="success" class="m-1">
+        Tag 1
+      </el-tag>
+      <el-tag type="warning" class="m-1">
+        Tag 1
+      </el-tag>
+      <el-tag type="danger" class="m-1">
+        Tag 1
+      </el-tag>
+      <el-tag type="info" class="m-1">
+        Tag 1
+      </el-tag>
+    </div>
 
-  <div>
-    <el-switch v-model="value1" />
-    <el-switch v-model="value1" class="m-2" style="--ep-switch-on-color: black; --ep-switch-off-color: gray;" />
-  </div>
+    <div>
+      <el-switch v-model="value1" />
+      <el-switch v-model="value1" class="m-2" style="--ep-switch-on-color: black; --ep-switch-off-color: gray;" />
+    </div>
 
-  <div class="my-2">
-    <el-input v-model="input" class="m-2" style="width: 200px" />
-    <el-date-picker v-model="curDate" class="m-2" type="date" placeholder="Pick a day" />
+    <div class="my-2">
+      <el-input v-model="input" class="m-2" style="width: 200px" />
+      <el-date-picker v-model="curDate" class="m-2" type="date" placeholder="Pick a day" />
+    </div>
   </div>
 </template>
 
